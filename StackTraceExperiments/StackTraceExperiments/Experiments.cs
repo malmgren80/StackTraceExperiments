3a7d7c266a74fec9130efb0edddd63d4d067c16a
malmgren80 <marcusmalmgren@hotmail.com>
2014-02-11 23:09:58 +0100

    Added project

---
+++ StackTraceExperiments/StackTraceExperiments/Experiments.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using StackTraceExperiments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTraceExperiments
+{
+    [TestFixture]
+    public class InvokerMethodInfoTests
+    {
+        [Test]
+        public void Should_get_invoker_method_when_called_from_method()
+        {
+            var invoker = new MethodInvoker();
+
+            invoker.Execute();
+
+            Assert.That(invoker.MethodInfo.ToString(), Is.EqualTo("StackTraceExperiments.InvokerMethodInfoTests.Should_get_invoker_method_when_called_from_method()"));
+        }
+
+        [Test]
+        public void Should_get_property_when_called_from_property()
+        {
+            string actual = PropertyInvoker.ToString();
+
+            Assert.That(actual, Is.EqualTo("StackTraceExperiments.InvokerMethodInfoTests.get_PropertyInvoker()"));
+        }
+
+        [Test]
+        public void Should_get_invoker_method_when_called_from_method_group()
+        {
+            var invoker = new MethodInvoker();
+            Action action = invoker.Execute;
+
+            action.Invoke();
+
+            Assert.That(invoker.MethodInfo.ToString(), Is.EqualTo("StackTraceExperiments.InvokerMethodInfoTests.Should_get_invoker_method_when_called_from_method_group()"));
+        }
+
+        [Test]
+        public void Should_get_invoker_method_and_anonymous_description_when_called_from_anonymous_method()
+        {
+            var invoker = new MethodInvoker();
+            Action action = delegate() { invoker.Execute(); };
+
+            action.Invoke();
+
+            Assert.That(invoker.MethodInfo.ToString(), Is.EqualTo("StackTraceExperiments.InvokerMethodInfoTests.Should_get_invoker_method_and_anonymous_description_when_called_from_anonymous_method() --- () => {...}"));
+        }
+
+        [Test]
+        public void Should_get_invoker_method_and_anonymous_description_when_called_from_lambda()
+        {
+            var invoker = new MethodInvoker();
+            Action action = () => invoker.Execute();
+
+            action.Invoke();
+
+            Assert.That(invoker.MethodInfo.ToString(), Is.EqualTo("StackTraceExperiments.InvokerMethodInfoTests.Should_get_invoker_method_and_anonymous_description_when_called_from_lambda() --- () => {...}"));
+        }
+
+        [Test]
+        public void Should_get_invoker_method_and_anonymous_description_when_called_from_lambda_in_lambda()
+        {
+            var invoker = new MethodInvoker();
+            Action outer = () => 
+            {
+                Action inner = () => invoker.Execute();
+                inner.Invoke();
+            };
+
+            outer.Invoke();
+
+            Assert.That(invoker.MethodInfo.ToString(), Is.EqualTo("StackTraceExperiments.InvokerMethodInfoTests.Should_get_invoker_method_and_anonymous_description_when_called_from_lambda_in_lambda() --- () => {...}"));
+        }
+
+        public InvokerMethodInfo PropertyInvoker
+        {
+            get
+            {
+                var invoker = new MethodInvoker();
+                invoker.Execute();
+                return invoker.MethodInfo;
+            }
+        }
+
+        class MethodInvoker
+        {
+            public InvokerMethodInfo MethodInfo;
+
+            public void Execute()
+            {
+                MethodInfo = new InvokerMethodInfo();
+            }
+        }
+    }
+}
---
+++ StackTraceExperiments/StackTraceExperiments/InvokerMethodInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackTraceExperiments
+{
+    public class InvokerMethodInfo
+    {
+        private readonly StackTrace _trace;
+        private const int FramesToSkip = 2;
+        private const string AnonymousMethodDescription = " --- () => {...}";
+
+        public InvokerMethodInfo()
+        {
+            _trace = new StackTrace(FramesToSkip, false);
+        }
+
+        private string GetDescription(MethodBase method, bool isCalledFromAnonymous)
+        {
+            string fullMethodName = string.Format("{0}.{1}()", method.DeclaringType.FullName, method.Name);
+            if (isCalledFromAnonymous)
+            {
+                return string.Concat(fullMethodName, AnonymousMethodDescription);
+            }
+            return fullMethodName;
+        }
+
+        public override string ToString()
+        {
+            int index = 0;
+            while (index < _trace.FrameCount)
+            {
+                var frame = _trace.GetFrame(index);
+
+                if (!IsAnonymousMethod(frame))
+                {
+                    var method = frame.GetMethod();
+                    return GetDescription(method, index != 0);
+                }
+
+                index++;
+            }
+
+            return string.Empty;
+        }
+
+        private bool IsAnonymousMethod(StackFrame frame)
+        {
+            /// Tried this one as in SO answer, but no CustomAttributes found
+            /// return frame.GetMethod().GetCustomAttributes(typeof(CompilerGeneratedAttribute)).Any();
+            /// http://stackoverflow.com/questions/2503336/how-to-identify-anonymous-methods-in-system-reflection
+
+            // Don't know if this will work in all cases...
+
+            return frame.ToString().StartsWith("<");
+        }
+    }
+}
